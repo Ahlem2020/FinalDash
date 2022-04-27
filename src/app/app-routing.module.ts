7e4db05d3e11fdd82e811de0f5938fe503b3f496
Ahlem2020 <ahlem.benfradj@esprit.tn>
2022-04-27 05:36:09 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashComponent } from './Components/dash/dash.component';
+import { SubjectComponent } from './Components/subject/subject.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'dash' , component: DashComponent
+  
+  },
+  {
+    path: 'subject', component : SubjectComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DashComponent } from './Components/dash/dash.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SubjectComponent } from './Components/subject/subject.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashComponent,
+    FooterComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
